5f642e1bcf8a2afcdbdb05634e3cd7d01f84e497
Thomas Hexton <25544371+thomashexton@users.noreply.github.com>
2024-12-23 10:16:27 +1100

    bracket combos working on base layer

--- keyboards/bastardkb/charybdis/3x5/v2/splinky_3/keymaps/thomashexton/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/v2/splinky_3/keymaps/thomashexton/keymap.c
@@ -14,9 +14,6 @@ static uint16_t auto_pointer_layer_timer = 0;
 
 #ifndef POINTING_DEVICE_ENABLE
 #    define DRGSCRL XXXXXXX
-#    define DPI_MOD XXXXXXX
-#    define S_D_MOD XXXXXXX
-#    define SNIPING XXXXXXX
 #endif // !POINTING_DEVICE_ENABLE
 
 #define PT_TOGG TG(LAYER_POINTER)
@@ -32,39 +29,43 @@ static uint16_t auto_pointer_layer_timer = 0;
 #define THUML1 LT(LAYER_RAISE, KC_SPC)
 #define THUML2 LT(LAYER_LOWER, KC_TAB)
 #define THUML3 LT(LAYER_POINTER, KC_ESC)
-#define THUMR1 LT(LAYER_RAISE, KC_BSPC)
-#define THUMR2 LT(LAYER_LOWER, KC_ENT)
+#define THUMR1 LT(LAYER_RAISE, KC_ENT)
+#define THUMR2 LT(LAYER_LOWER, KC_BSPC)
 // #define THUMR3 KC_NO // LT(LAYER_FUNCTION, KC_DEL)
 
 #define _L_PTR(KC) LT(LAYER_POINTER, KC)
 
 /* ────────────────────────────────────────────────────────────────────────── *
  *  FIXED COMBOS
- *
- *  Instead of RCTL_T(KC_N) or RSFT_T(KC_E), we use plain KC_N, KC_E, etc.
  * ────────────────────────────────────────────────────────────────────────── */
 const uint16_t PROGMEM o_curly_brace_combo[] = {RCTL_T(KC_N), RSFT_T(KC_E), COMBO_END};
 const uint16_t PROGMEM c_curly_brace_combo[] = {RSFT_T(KC_E), RALT_T(KC_I), COMBO_END};
+const uint16_t PROGMEM o_bracket_combo[]     = {KC_M, RCTL_T(KC_N), COMBO_END};
+const uint16_t PROGMEM c_bracket_combo[]     = {RALT_T(KC_I), RGUI_T(KC_O), COMBO_END};
 const uint16_t PROGMEM o_paren_combo[]       = {KC_H, KC_COMM, COMBO_END};
 const uint16_t PROGMEM c_paren_combo[]       = {KC_COMM, KC_DOT, COMBO_END};
+const uint16_t PROGMEM o_angle_combo[]       = {KC_K, KC_H, COMBO_END};
+const uint16_t PROGMEM c_angle_combo[]       = {KC_DOT, _L_PTR(KC_SLSH), COMBO_END};
 const uint16_t PROGMEM plus_combo[]          = {KC_P, KC_L, COMBO_END};
 const uint16_t PROGMEM minus_combo[]         = {LCTL_T(KC_T), RCTL_T(KC_N), COMBO_END};
-const uint16_t PROGMEM equals_combo[]         = {KC_G, KC_M, COMBO_END};
+const uint16_t PROGMEM equal_combo[]         = {KC_G, KC_M, COMBO_END};
 const uint16_t PROGMEM under_combo[]         = {KC_D, KC_H, COMBO_END};
 
 /* ────────────────────────────────────────────────────────────────────────── *
  *  COMBO ARRAY
- *
- *  We only list the combos we’re actually using. That means 7 combos total.
  * ────────────────────────────────────────────────────────────────────────── */
 combo_t key_combos[] = {
     COMBO(o_curly_brace_combo, KC_LEFT_CURLY_BRACE),
     COMBO(c_curly_brace_combo, KC_RIGHT_CURLY_BRACE),
+    COMBO(o_bracket_combo, KC_LEFT_BRACKET),
+    COMBO(c_bracket_combo, KC_RIGHT_BRACKET),
     COMBO(o_paren_combo, KC_LEFT_PAREN),
     COMBO(c_paren_combo, KC_RIGHT_PAREN),
+    COMBO(o_angle_combo, KC_LEFT_ANGLE_BRACKET),
+    COMBO(c_angle_combo, KC_RIGHT_ANGLE_BRACKET),
     COMBO(plus_combo, KC_PLUS),
     COMBO(minus_combo, KC_MINUS),
-    COMBO(equals_combo, KC_EQUALS),
+    COMBO(equal_combo, KC_EQUAL),
     COMBO(under_combo, KC_UNDERSCORE),
 };
 
@@ -73,11 +74,14 @@ combo_t key_combos[] = {
  * ────────────────────────────────────────────────────────────────────────── */
 uint8_t COMBO_COUNT = sizeof(key_combos) / sizeof(key_combos[0]);
 
+/* ────────────────────────────────────────────────────────────────────────── *
+ *  LAYER ARRAYS
+ * ────────────────────────────────────────────────────────────────────────── */
 #define COLEMAK_DH_LAYER                                                                \
             KC_Q, KC_W, KC_F, KC_P, KC_B, KC_J, KC_L, KC_U, KC_Y, KC_QUOT,              \
         ____GACS_L____(A, R, S, T), KC_G, KC_M, ____GACS_R____(N, E, I, O),             \
     _L_PTR(KC_Z), KC_X, KC_C, KC_D, KC_V, KC_K, KC_H, KC_COMM, KC_DOT, _L_PTR(KC_SLSH), \
-                  THUML3, THUML1, THUML2, THUMR2, THUMR1
+                  THUML3, THUML1, THUML2, THUMR1, THUMR2
 
 #define RAISE_LAYER                                                                           \
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
@@ -93,7 +97,7 @@ uint8_t COMBO_COUNT = sizeof(key_combos) / sizeof(key_combos[0]);
 
 #define POINTER_LAYER                                                                         \
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+    XXXXXXX, XXXXXXX, DRGSCRL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
     _______, KC_BTN2, KC_BTN3, KC_BTN1, XXXXXXX, XXXXXXX, KC_BTN1, KC_BTN3, KC_BTN2, _______, \
                       _______, KC_BTN1, KC_BTN3, KC_BTN3, KC_BTN1
 
@@ -148,12 +152,12 @@ float scroll_accumulated_v = 0;
     }
 #endif // POINTING_DEVICE_ENABLE
 
-layer_state_t layer_state_set_user(layer_state_t state) {
-    // Enable set_scrolling if the current layer is the POINTER_LAYER
-    if (get_highest_layer(state) == LAYER_POINTER) {
-        set_scrolling = true;
-    } else {
-        set_scrolling = false;
-    }
-    return state;
-}
+// layer_state_t layer_state_set_user(layer_state_t state) {
+//     // Enable set_scrolling if the current layer is the POINTER_LAYER
+//     if (get_highest_layer(state) == LAYER_POINTER) {
+//         set_scrolling = true;
+//     } else {
+//         set_scrolling = false;
+//     }
+//     return state;
+// }
