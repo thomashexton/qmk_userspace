5d197143ba80f82c1777eec5f89ac033310a91d3
Thomas Hexton <25544371+thomashexton@users.noreply.github.com>
2025-04-23 20:33:09 +1000

    update back to += vert scroll

--- users/thomashexton/thomashexton.c
+++ users/thomashexton/thomashexton.c
@@ -97,7 +97,7 @@ report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
     if (set_scrolling) {
         // Calculate and accumulate scroll values based on mouse movement and divisors
         scroll_accumulated_h += (float)mouse_report.x / SCROLL_DIVISOR_H;
-        scroll_accumulated_v -= (float)mouse_report.y / SCROLL_DIVISOR_V;
+        scroll_accumulated_v += (float)mouse_report.y / SCROLL_DIVISOR_V;
 
         // Assign integer parts of accumulated scroll values to the mouse report
         mouse_report.h = (int8_t)scroll_accumulated_h;
